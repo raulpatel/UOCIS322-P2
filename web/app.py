fd21ae6b574be52414ebeb1c6569499ca3c7a371
Raul Patel <raulp@testium.cs.uoregon.edu>
2021-04-25 19:32:01 -0700

    added files for testing

--- web/app.py
+++ web/app.py
@@ -2,13 +2,37 @@
 John Doe's Flask API.
 """
 
-from flask import Flask
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "UOCIS docker demo!\n"
+
+@app.route("/hello")
+def new():
+    return "Hello there friend.\n"
+
+@app.route("/<path:name>")
+def page_render(name):
+    parts = name.split('.')
+    if parts[-1] != "html" or parts[-1] != "css":
+        abort(403)
+    try:
+        with open(name, "r") as f:
+            pass
+    except FileNotFoundError:
+        abort(404)
+    return render_template("/../{}".format(name))
+
+@app.errorhandler(404)
+def error_404(404):
+    return render_template("404.html")
+
+@app.errorhandler(403)
+def error_404(403):
+    return render_template("403.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
